43912995c23928740fc0c072911ca120bebd08f3
Bailey Hill <bhill1@mail.sfsu.edu>
2020-01-22 10:08:01 -0800

    Updated match two cards feature

--- main.js
+++ main.js
@@ -5,6 +5,17 @@ function handleClick(event) {
   if(event.target.className.indexOf("card-back") === -1) {
     return;
   }
-  console.log(event);
+  // console.log(event);
   event.target.className = "hidden";
+ if(firstCardClicked == true) {
+   console.log(secondCardClicked);
+   secondCardClicked = document.getElementsByClassName("card-back");
+ } else {
+   console.log(firstCardClicked);
+  firstCardClicked = document.getElementsByClassName("card-back");
+ }
 }
+var firstCardClicked;
+var secondCardClicked;
+var firstCardClasses;
+var secondCardClasses;
