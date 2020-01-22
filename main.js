fdea3b79f1533293745a15228bc67fd9f9ee5330
Bailey Hill <bhill1@mail.sfsu.edu>
2020-01-21 17:48:27 -0800

    Completed user-can-flip-cards

---
+++ main.js
@@ -0,0 +1,10 @@
+var gameCards = document.querySelector("#gameCards");
+gameCards.addEventListener("click", handleClick);
+
+function handleClick(event) {
+  if(event.target.className.indexOf("card-back") === -1) {
+    return;
+  }
+  console.log(event);
+  event.target.className = "hidden";
+}
